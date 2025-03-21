38c73ebb02c9392221cd736251d97ce10a091a47
Alex Burnet <alexburnet03@gmail.com>
2025-03-21 17:55:46 +0000

    Set admin password from .env

--- server/startup.py
+++ server/startup.py
@@ -1,6 +1,12 @@
 from app import app, db, User
 from flask_migrate import migrate, upgrade
 from sqlalchemy.exc import OperationalError
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
 
 def ensure_admin():
     email = "admin@alexbur.net"
@@ -16,7 +22,7 @@ def ensure_admin():
     else:
         print(f"➕ Creating new admin account: {email}")
         user = User(email=email, is_admin=True)
-        user.set_password("changeme123")  # You may want to rotate this
+        user.set_password(ADMIN_PASSWORD)  # You may want to rotate this
         db.session.add(user)
         db.session.commit()
         print("✅ Admin user created.")
