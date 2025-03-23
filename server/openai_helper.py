9b5cf782ced4e6555ad5ee10035c84d857a9044a
Alex Burnet <alexburnet03@gmail.com>
2025-03-23 21:15:45 +0000

    remove max tokens

--- server/openai_helper.py
+++ server/openai_helper.py
@@ -32,7 +32,6 @@ Session Notes:
             {"role": "user", "content": prompt}
         ],
         temperature=0.7,
-        max_tokens=2000
     )
 
     return completion.choices[0].message.content
