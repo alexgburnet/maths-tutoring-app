9ec6c13d06a56be2cca6fc60cef8a79882f7fc16
Alex Burnet <alexburnet03@gmail.com>
2025-03-26 18:53:24 +0000

    Added styling to booking card

--- client/src/components/BookingCard/BookingCard.jsx
+++ client/src/components/BookingCard/BookingCard.jsx
@@ -1,29 +1,53 @@
 import './BookingCard.css';
 
 export default function BookingCard({ booking, isUpcoming, cancelBooking }) {
-
-
     return (
         <div className="booking-card" key={booking.id}>
-            <h3 className="booking-header">{booking.topic}</h3>
-            <div class="vertical-line"></div>
-            <p><strong>Date:</strong> {new Date(booking.scheduled_time).toLocaleString()}</p>
-            <div class="vertical-line"></div>
-            <p><strong>Zoom:</strong> {booking.zoom_link ? <a href={booking.zoom_link} target="_blank" rel="noreferrer">Join Meeting</a> : "—"}</p>
-            <div class="vertical-line"></div>
-            <p><strong>Payment Ref:</strong> {booking.payment_ref}</p>
-            <div class="vertical-line"></div>
+
+            <div className="topic card-section">
+                <p>topic</p>
+                <h3 className="booking-header">{booking.topic}</h3>
+            </div>
+
+            <div className="vertical-line"></div>
+
+            <div className="date card-section">
+                <p>date</p>
+                <h3>{new Date(booking.scheduled_time).toLocaleString()}</h3>
+            </div>
+
+            <div className="vertical-line"></div>
+
+            <div className="zoom card-section">
+                <p>zoom link</p>
+                <h3>{booking.zoom_link ? <a href={booking.zoom_link} target="_blank" rel="noreferrer">Join Meeting</a> : "—"}</h3>
+            </div>
+
+            <div className="vertical-line"></div>
+
+            <div className="reference card-section">
+                <p>payment ref:</p>
+                <h3>{booking.payment_ref}</h3>
+            </div>
+
+            <div className="vertical-line"></div>
+
             {isUpcoming && (
                 <>
-                    <button className="cancel-button" onClick={() => cancelBooking(booking.id)}>
-                        Cancel Booking
-                    </button>
-                    <div class="vertical-line"></div>
+                    <div className="cancel card-section">
+                        <button className="cancel-button" onClick={() => cancelBooking(booking.id)}>
+                            Cancel Booking
+                        </button>
+                    </div>
+                    <div className="vertical-line"></div>
                 </>
             )}
-            <span className={`status ${booking.is_paid ? "paid" : "unpaid"}`}>
+
+            <div className="payment card-section">
+                <span className={`status ${booking.is_paid ? "paid" : "unpaid"}`}>
                     {booking.is_paid ? "Paid" : "Unpaid"}
-            </span>
+                </span>
+            </div>
         </div>
     );
 }
